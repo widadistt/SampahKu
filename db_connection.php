687136863f3a90e45f7d514f22ad0f96334aaa9e
Widad Istiqomah <18218011@std.stei.itb.ac.id>
2021-01-02 22:49:53 +0700

    fix: db

--- db_connection.php
+++ db_connection.php
@@ -2,20 +2,19 @@
     session_start();
     session_regenerate_id(true);
 
-    // $host = "ec2-52-201-55-4.compute-1.amazonaws.com";
-    // $user = "tsksabxokdawqm";
-    // $password = "03f55d605a04fa4d25ee18b978c211953141532f2196333a6a7ed0e4862d0744";
-    // $dbname = "d4h2ac7l9anakb";
+    $host = "sql12.freemysqlhosting.net";
+    $user = "sql12384697";
+    $password = "wqLDnFyuNG";
+    $dbname = "sql12384697";
 
     // change the information according to your database
-    // $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
-    // $db_connection = mysqli_connect("$host","$user","$password","$dbname");
+    $db_connection = mysqli_connect("$host","$user","$password","$dbname");
 
-    $db = parse_url(getenv("postgres://tsksabxokdawqm:03f55d605a04fa4d25ee18b978c211953141532f2196333a6a7ed0e4862d0744@ec2-52-201-55-4.compute-1.amazonaws.com:5432/d4h2ac7l9anakb"));
-    $db["path"] = ltrim($db["path"], "/");
-    
+    // $db = parse_url(getenv("postgres://tsksabxokdawqm:03f55d605a04fa4d25ee18b978c211953141532f2196333a6a7ed0e4862d0744@ec2-52-201-55-4.compute-1.amazonaws.com:5432/d4h2ac7l9anakb"));
+    // $db["path"] = ltrim($db["path"], "/");
+
     // CHECK DATABASE CONNECTION
     if(mysqli_connect_errno()){
         echo "Connection Failed".mysqli_connect_error();
         exit;
-}
+    }
