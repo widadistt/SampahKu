61c2a0609fcbaed57b2d1d0d8fcb8d5a85b4d721
Widad Istiqomah <18218011@std.stei.itb.ac.id>
2021-01-02 21:38:44 +0700

    fix: db

--- db_connection.php
+++ db_connection.php
@@ -2,15 +2,17 @@
     session_start();
     session_regenerate_id(true);
 
-    $host = "ec2-52-201-55-4.compute-1.amazonaws.com";
-    $user = "tsksabxokdawqm";
-    $password = "03f55d605a04fa4d25ee18b978c211953141532f2196333a6a7ed0e4862d0744";
-    $dbname = "d4h2ac7l9anakb";
+    // $host = "ec2-52-201-55-4.compute-1.amazonaws.com";
+    // $user = "tsksabxokdawqm";
+    // $password = "03f55d605a04fa4d25ee18b978c211953141532f2196333a6a7ed0e4862d0744";
+    // $dbname = "d4h2ac7l9anakb";
 
     // change the information according to your database
-    $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
-    $db_connection = mysqli_connect("$host","$user","$password","$dbname");
+    // $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
+    // $db_connection = mysqli_connect("$host","$user","$password","$dbname");
 
+    $db = parse_url(getenv("postgres://tsksabxokdawqm:03f55d605a04fa4d25ee18b978c211953141532f2196333a6a7ed0e4862d0744@ec2-52-201-55-4.compute-1.amazonaws.com:5432/d4h2ac7l9anakb"));
+    $db["path"] = ltrim($db["path"], "/");
     // CHECK DATABASE CONNECTION
     if(mysqli_connect_errno()){
         echo "Connection Failed".mysqli_connect_error();
